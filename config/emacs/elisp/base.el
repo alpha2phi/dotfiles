0a44978f83b88c13b81696a25838c0a52d21a0fe
Meng Wang <mengwangk@gmail.com>
2021-02-17 22:48:18 +0800

    Initial commit

---
+++ config/emacs/elisp/base.el
@@ -0,0 +1,37 @@
+;; Set default font size
+(defvar alpha2phi/default-font-size 160)
+(set-face-attribute 'default nil :height alpha2phi/default-font-size)
+
+;; Package repositories
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Define private and temp directories
+; (defconst private-dir  (expand-file-name "private" user-emacs-directory))
+; (defconst temp-dir (format "%s/cache" private-dir)  "elisp temp directories")
+
+;; Emacs customizations
+(setq custom-file                        "~/.config/emacs/custom.el"
+      ; inhibit-startup-message          t
+      use-package-always-ensure          t)
+
+(show-paren-mode 1)
+
+;; Need to load custom file to avoid being overwritten
+;; more at https://www.gnu.org/software/emacs/manual/html_node/emacs/Saving-Customizations.html
+(load custom-file)
+
+;; Delete trailing whitespace before save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(provide 'base)
--- config/emacs/init.el
+++ config/emacs/init.el
@@ -2,21 +2,3 @@
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 
 (require 'base)
-
-(require 'base-theme)
-(require 'base-extensions)
-
-; (require 'base-functions)
-; (require 'base-global-keys)
-
-; (require 'lang-python)
-
-; (require 'lang-go)
-
-; (require 'lang-javascript)
-
-; (require 'lang-web)
-
-; (require 'lang-rust)
-
-; (require 'lang-c)
