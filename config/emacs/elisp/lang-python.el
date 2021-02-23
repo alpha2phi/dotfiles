3775af9071118d2b23e5e49ef512d644d240116a
Meng Wang <mengwangk@gmail.com>
2021-02-23 14:34:49 +0800

    Initial commit

--- config/emacs/elisp/lang-python.el
+++ config/emacs/elisp/lang-python.el
@@ -2,6 +2,7 @@
   :hook (python-mode . (lambda ()
                         (require 'lsp-pyright)
                         (lsp-deferred))))
+                        ; (lsp))))
 
 
 ;;; lang python
--- config/emacs/elisp/lang.el
+++ config/emacs/elisp/lang.el
@@ -8,16 +8,25 @@
   (setq lsp-keymap-prefix "C-c l")
   :hook (
          (python-mode . lsp-deferred)
+         ; (python-mode . lsp)
          (lsp-mode . lsp-enable-which-key-integration))
   :commands (lsp lsp-deferred))
+  ; :commands (lsp))
 
-(use-package lsp-ui :commands lsp-ui-mode)
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
 
 ;; DAP
 (use-package dap-mode)
 ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
 
 ;; base lang
-provide 'lang)
+(provide 'lang)
