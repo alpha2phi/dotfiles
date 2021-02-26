cda9a7c5905beac0fd8c358c4a007fa4e225bb35
Meng Wang <mengwangk@gmail.com>
2021-02-26 21:48:02 +0800

    Initial commit

--- config/emacs/elisp/keymappings.el
+++ config/emacs/elisp/keymappings.el
@@ -9,9 +9,12 @@
   (alpha2phi/leader-keys
     "t"  '(:ignore t :which-key "toggles")
     "tt" '(counsel-load-theme :which-key "choose theme")
-    "p"  '(:ignore p :which-key "toggles")
+    "p"  '(:ignore t :which-key "toggles")
     "pp" '(counsel-projectile-switch-project :which-key "choose project")
     "pc" '(projectile-command-map :which-key "choose action")
+    "f"  '(:ignore t :which-key "toggles")
+    "ff" '(counsel-fzf :which-key "fuzzy find")
+    "fx" '(counsel-M-x :which-key "M-x")
     ))
 
 (provide 'keymappings)
