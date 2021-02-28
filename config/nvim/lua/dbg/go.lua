1f9a99d05026ad575c3df1f87430dfd7db83d259
Meng Wang <mengwangk@gmail.com>
2021-02-28 11:38:35 +0800

    Initial commit

--- config/nvim/lua/dbg/go.lua
+++ config/nvim/lua/dbg/go.lua
@@ -24,7 +24,7 @@ dap.adapters.go = function(callback, config)
     --end,
     --100)
 
-    dap.repl.open()
+    -- dap.repl.open()
     callback({type = "server", host = "127.0.0.1", port = port})
 end
 -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
--- config/nvim/lua/dbg/rust.lua
+++ config/nvim/lua/dbg/rust.lua
@@ -1,13 +1,22 @@
 local dap = require "dap"
-dap.adapters.cpp = {
+dap.adapters.rust = {
     type = 'executable',
     attach = {
         pidProperty = "pid",
         pidSelect = "ask"
     },
-    command = 'lldb', -- my binary was called 'lldb-vscode-11'
+    command = 'lldb-vscode', -- my binary was called 'lldb-vscode-11'
     env = {
         LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"
     },
     name = "lldb"
+}
+
+dap.configurations.rust = {
+    {
+        type = "rust",
+        name = "Debug",
+        request = "launch",
+        program= "/Users/mengwangk/workspace/development/rust-app/target/debug/myapp1"
+    }
 }
