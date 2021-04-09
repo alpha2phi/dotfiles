b3f813e5787f6357ac69fde8700664a1bc84970e
Meng Wang <mengwangk@gmail.com>
2021-04-09 18:57:55 +0800

    Update org mode font

--- config/emacs/elisp/organize.el
+++ config/emacs/elisp/organize.el
@@ -1,6 +1,30 @@
+;;; organize.el --- org mode configuration
+
+;;; Commentary:
+;; All org mode configurations should be here
+
+;;; Code:
+
+
+(defun alpha2phi/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun alpha2phi/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•")))))))
+
+
 (use-package org
   :pin org
   :commands (org-capture org-agenda)
+  :hook (org-mode . alpha2phi/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (alpha2phi/org-font-setup)
 )
 
 (use-package org-bullets
@@ -10,7 +34,7 @@
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 ;; Set faces for heading levels
-(dolist (face '((org-level-1 . 1.2)
+(with-eval-after-load 'org-faces (dolist (face '((org-level-1 . 1.2)
 		(org-level-2 . 1.1)
 		(org-level-3 . 1.05)
 		(org-level-4 . 1.0)
@@ -18,7 +42,13 @@
 		(org-level-6 . 1.1)
 		(org-level-7 . 1.1)
 		(org-level-8 . 1.1)))
-(set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+(set-face-attribute (car face) nil :weight 'regular :height (cdr face)))
+)
+
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((python . t)))
 
 
 (provide 'organize)
+;;; organize.el ends here
