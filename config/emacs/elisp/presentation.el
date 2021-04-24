6dc375de1fa597ca5448deffd263ddc6d2ac10a0
Meng Wang <mengwangk@gmail.com>
2021-04-24 23:49:49 +0800

    Update Emacs config

--- config/emacs/elisp/presentation.el
+++ config/emacs/elisp/presentation.el
@@ -15,6 +15,14 @@
   (org-tree-slide-skip-outline-level 0)
   (org-image-actual-width nil))
 
+(use-package ox-reveal
+  :custom
+  (org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js")
+  (org-reveal-mathjax t)
+
+)
+
+(use-package htmlize)
 
 (provide 'presentation)
 ;;; presentation.el ends here
