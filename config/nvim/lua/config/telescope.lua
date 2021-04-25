6fd55a3f5eb6568f502dbe3dfe35ed9a052e639f
Meng Wang <mengwangk@gmail.com>
2021-04-25 19:37:00 +0800

    Update telescope

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -1,4 +1,12 @@
 require('telescope').setup {
+    find_command = {
+        'rg', '--no-heading', '--with-filename', '--line-number', '--column',
+        '--smart-case'
+    },
+    use_less = true,
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
     extensions = {
         arecibo = {
             ["selected_engine"] = 'google',
