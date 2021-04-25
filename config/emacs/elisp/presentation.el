0ad532c5022929c313e55b52b60e0adcac4baab1
Meng Wang <mengwangk@gmail.com>
2021-04-25 09:52:49 +0800

    Update presentation.el

--- config/emacs/elisp/presentation.el
+++ config/emacs/elisp/presentation.el
@@ -21,8 +21,43 @@
   (org-reveal-mathjax t)
 
 )
-
+;; for syntax highlightng
 (use-package htmlize)
+
+(defun alpha2phi/org-present-prepare-slide ()
+  "Prepare slide."
+  (org-overview)
+  (org-show-entry)
+  (org-show-children))
+
+(defun alpha2phi/org-present-prev ()
+  "Previous slide."
+  (interactive)
+  (org-present-prev)
+  (alpha2phi/org-present-prepare-slide))
+
+(defun alpha2phi/org-present-next ()
+  "Next slide."
+  (interactive)
+  (org-present-next)
+  (alpha2phi/org-present-prepare-slide))
+
+(use-package org-present
+  :bind (:map org-present-mode-keymap
+         ("C-c C-j" . alpha2phi/org-present-next)
+         ("C-c C-k" . alpha2phi/org-present-prev))
+  :hook (
+	 (org-present-mode . (lambda ()
+                 (org-present-big)
+                 (org-display-inline-images)
+                 (org-present-hide-cursor)
+                 (org-present-read-only)))
+         (org-present-mode-quit . (lambda ()
+                 (org-present-small)
+                 (org-remove-inline-images)
+                 (org-present-show-cursor)
+                 (org-present-read-write)))
+	))
 
 (provide 'presentation)
 ;;; presentation.el ends here
