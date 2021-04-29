8f8d21619d3a5f9179558dda4c02f8bedc27b6a9
Meng Wang <mengwangk@gmail.com>
2021-04-29 19:10:00 +0800

    Update EAF

--- config/emacs/elisp/desktop.el
+++ config/emacs/elisp/desktop.el
@@ -1,9 +1,23 @@
-;;; desktop.el --- Desktop and Windows Manager
+;;; desktop.el --- Desktop management
 ;;; Commentary:
-;;; Desktop and Windows manager
+;;; Desktop management
 
 ;;; Code:
-
+(use-package eaf
+  :load-path "~/.config/emacs/emacs-application-framework" 
+  :init
+  (use-package epc :defer t :ensure t)
+  (use-package ctable :defer t :ensure t)
+  (use-package deferred :defer t :ensure t)
+  (use-package s :defer t :ensure t)
+  :custom
+  (eaf-browser-continue-where-left-off t)
+  :config
+  (eaf-setq eaf-browser-enable-adblocker "true")
+  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding)) 
 
 
 (provide 'desktop)
