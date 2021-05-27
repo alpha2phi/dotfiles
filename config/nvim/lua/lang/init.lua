cc911b39c160aad1773bbbbd03c32eeed7baac46
Meng Wang <mengwangk@gmail.com>
2021-05-27 23:48:22 +0800

    Update LSP

--- config/nvim/lua/lang/init.lua
+++ config/nvim/lua/lang/init.lua
@@ -161,3 +161,34 @@ vim.g.symbols_outline = {
     },
     lsp_blacklist = {}
 }
+
+-- LSP Enable diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false,
+        underline = true,
+        signs = true,
+        update_in_insert = false
+    })
+
+-- Send diagnostics to quickfix list
+do
+    local method = "textDocument/publishDiagnostics"
+    local default_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, method, result, client_id, bufnr,
+                                        config)
+        default_handler(err, method, result, client_id, bufnr, config)
+        local diagnostics = vim.lsp.diagnostic.get_all()
+        local qflist = {}
+        for bufnr, diagnostic in pairs(diagnostics) do
+            for _, d in ipairs(diagnostic) do
+                d.bufnr = bufnr
+                d.lnum = d.range.start.line + 1
+                d.col = d.range.start.character + 1
+                d.text = d.message
+                table.insert(qflist, d)
+            end
+        end
+        vim.lsp.util.set_qflist(qflist)
+    end
+end
