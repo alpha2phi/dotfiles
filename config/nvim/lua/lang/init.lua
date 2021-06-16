3133cf2988c3f394c721fcf8cc226c2f37570d50
Meng Wang <mengwangk@gmail.com>
2021-06-16 14:38:29 +0800

    Update LSP config

--- config/nvim/lua/lang/init.lua
+++ config/nvim/lua/lang/init.lua
@@ -165,13 +165,13 @@ vim.g.symbols_outline = {
 }
 
 -- LSP Enable diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = false,
-        underline = true,
-        signs = true,
-        update_in_insert = false
-    })
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] =
+--     vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+--         virtual_text = false,
+--         underline = true,
+--         signs = true,
+--         update_in_insert = false
+--     })
 
 -- Send diagnostics to quickfix list
 do
