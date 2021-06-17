756a6b5cf4b2e7d5f2494c3ea13d81d142c92703
Meng Wang <mengwangk@gmail.com>
2021-06-17 22:25:53 +0800

    Update debug config

--- config/nvim/lua/dbg/init.lua
+++ config/nvim/lua/dbg/init.lua
@@ -12,6 +12,7 @@ vim.g.dap_virtual_text = true
 require("dapui").setup()
 
 -- vim-ultest
+vim.g.ultest_use_pty = 1
 require("ultest").setup({
     builders = {
         ['python#unittest'] = function(cmd)
@@ -34,31 +35,31 @@ require("ultest").setup({
                     args = args
                 }
             }
-        end,
-        ["go#gotest"] = function(cmd)
-            local args = {}
-            for i = 3, #cmd - 1, 1 do
-                local arg = cmd[i]
-                if vim.startswith(arg, "-") then
-                    -- Delve requires test flags be prefix with 'test.'
-                    arg = "-test." .. string.sub(arg, 2)
-                end
-                args[#args + 1] = arg
-            end
-            return {
-                dap = {
-                    type = "go",
-                    request = "launch",
-                    mode = "test",
-                    program = "${workspaceFolder}",
-                    dlvToolPath = vim.fn.exepath("dlv"),
-                    args = args
-                },
-                parse_result = function(lines)
-                    return lines[#lines] == "FAIL" and 1 or 0
-                end
-            }
         end
+        -- ["go#gotest"] = function(cmd)
+        --     local args = {}
+        --     for i = 3, #cmd - 1, 1 do
+        --         local arg = cmd[i]
+        --         if vim.startswith(arg, "-") then
+        --             -- Delve requires test flags be prefix with 'test.'
+        --             arg = "-test." .. string.sub(arg, 2)
+        --         end
+        --         args[#args + 1] = arg
+        --     end
+        --     return {
+        --         dap = {
+        --             type = "go",
+        --             request = "launch",
+        --             mode = "test",
+        --             program = "${workspaceFolder}",
+        --             dlvToolPath = vim.fn.exepath("dlv"),
+        --             args = args
+        --         },
+        --         parse_result = function(lines)
+        --             return lines[#lines] == "FAIL" and 1 or 0
+        --         end
+        --     }
+        -- end
     }
 })
 
