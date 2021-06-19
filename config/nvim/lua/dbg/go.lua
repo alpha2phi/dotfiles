430d802ab9f52253d811ce2070de526fd4d5b233
Meng Wang <mengwangk@gmail.com>
2021-06-19 11:19:42 +0800

    Update new debug adapter

--- config/nvim/lua/dbg/go.lua
+++ config/nvim/lua/dbg/go.lua
@@ -1,38 +1,40 @@
 local dap = require "dap"
 
 dap.adapters.go = function(callback, config)
-    local handle
-    local pid_or_err
-    local port = 38697
-    handle, pid_or_err =
-    vim.loop.spawn(
-    "dlv",
-    {
-        args = {"dap", "-l", "127.0.0.1:" .. port},
-        detached = true
-    },
-    function(code)
-        handle:close()
-        print("Delve exited with exit code: " .. code)
-    end
-    )
-    ----should we wait for delve to start???
-    --vim.defer_fn(
-    --function()
-    --dap.repl.open()
-    --callback({type = "server", host = "127.0.0.1", port = port})
-    --end,
-    --100)
+    local handle, pid_or_err, port = nil, nil, 12346
 
-    dap.repl.open()
-    callback({type = "server", host = "127.0.0.1", port = port})
+    handle, pid_or_err = vim.loop.spawn("dlv", {
+        args = {"dap", "-l", "127.0.0.1:" .. port},
+        detached = true,
+        cwd = vim.loop.cwd()
+    }, vim.schedule_wrap(function(code)
+        handle:close()
+        print("Delve has exited with: " .. code)
+    end))
+
+    if not handle then error("FAILED:", pid_or_err) end
+
+    vim.defer_fn(function()
+        callback {type = "server", host = "127.0.0.1", port = port}
+    end, 100)
 end
--- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+
 dap.configurations.go = {
     {
         type = "go",
         name = "Debug",
         request = "launch",
-        program = "${file}"
+        showLog = true,
+        program = "${file}",
+        -- console = "externalTerminal",
+        dlvToolPath = vim.fn.exepath "dlv"
+    }, {
+        name = "Test Current File",
+        type = "go",
+        request = "launch",
+        showLog = true,
+        mode = "test",
+        program = ".",
+        dlvToolPath = vim.fn.exepath "dlv"
     }
 }
--- config/nvim/lua/dbg/rust.lua
+++ config/nvim/lua/dbg/rust.lua
@@ -1,22 +1,54 @@
 local dap = require "dap"
-dap.adapters.rust = {
+
+-- dap.adapters.rust = {
+--     type = 'executable',
+--     attach = {pidProperty = "pid", pidSelect = "ask"},
+--     command = 'lldb-vscode', -- my binary was called 'lldb-vscode-11'
+--     env = {LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"},
+--     name = "lldb"
+-- }
+
+-- dap.configurations.rust = {
+--     {
+--         type = "rust",
+--         name = "Debug",
+--         request = "launch",
+--         program = "/Users/mengwangk/workspace/development/rust-app/target/debug/myapp1"
+--     }
+-- }
+
+dap.adapters.lldb = {
     type = 'executable',
-    attach = {
-        pidProperty = "pid",
-        pidSelect = "ask"
-    },
-    command = 'lldb-vscode', -- my binary was called 'lldb-vscode-11'
-    env = {
-        LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"
-    },
+    command = 'lldb-vscode',
     name = "lldb"
 }
 
-dap.configurations.rust = {
+dap.configurations.cpp = {
     {
-        type = "rust",
-        name = "Debug",
+        name = "Launch",
+        type = "lldb",
         request = "launch",
-        program= "/Users/mengwangk/workspace/development/rust-app/target/debug/myapp1"
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/',
+                                'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        runInTerminal = false
     }
 }
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
