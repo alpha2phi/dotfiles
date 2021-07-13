d9a47180febc03105e37fcd5bb225b243fda2fb1
Meng Wang <mengwangk@gmail.com>
2021-07-13 09:15:50 +0800

    Update treesitter

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = {} -- list of language that will be disabled
