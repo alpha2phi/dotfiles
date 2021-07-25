6af04eadc919a04871bd6b0f9af4f1f9846574a1
alpha2phi <alpha2phi@gmail.com>
2021-07-25 11:40:19 +0800

    Update treesitter

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
         enable = true,
         disable = {}
