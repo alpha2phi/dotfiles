9e516a5585178aed090ba419b26346dedd2275da
alpha2phi <alpha2phi@gmail.com>
2021-07-25 17:42:49 +0800

    Update lsp

--- config/nvim/lua/lang/init.lua
+++ config/nvim/lua/lang/init.lua
@@ -109,6 +109,9 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- }
 
 capabilities.textDocument.completion.completionItem.snippetSupport = true;
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = {"documentation", "detail", "additionalTextEdits"}
+}
 
 -- LSPs
 local servers = {"pyright", "rust_analyzer", "gopls", "tsserver", "vimls"}
