9e92b03b7ad9c4ac02f65ebfaed8e0d64fc1ec36
alpha2phi <alpha2phi@gmail.com>
2021-09-06 10:12:55 +0800

    Update key bindings

--- config/nvim/lua/config/cmp.lua
+++ config/nvim/lua/config/cmp.lua
@@ -34,7 +34,9 @@ cmp.setup {
     },
     mapping = {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
