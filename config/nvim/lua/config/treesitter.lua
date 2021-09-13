3a395604991fd2fe1d0fd45cb4381314690ad2af
alpha2phi <alpha2phi@gmail.com>
2021-09-13 11:16:39 +0800

    Refactor WIP

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
 
-    require'nvim-treesitter.configs'.setup {
+    require('nvim-treesitter.configs').setup {
         ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
         highlight = {
             enable = true,
