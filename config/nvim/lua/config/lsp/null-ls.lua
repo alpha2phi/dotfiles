45e85ac19fd5e0d9eb330b6729018377e5337e83
alpha2phi <alpha2phi@gmail.com>
2021-09-14 12:06:00 +0800

    Refactoring in progress

--- config/nvim/lua/config/lsp/null-ls.lua
+++ config/nvim/lua/config/lsp/null-ls.lua
@@ -1,23 +1,23 @@
 local M = {}
 
-local lsputils = require("config.lsp.utils")
+local lsputils = require "config.lsp.utils"
 
 CONFIG = {}
 
 function M.setup()
-    local null_ls = require("null-ls")
-    local sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.write_good
-            .with({filetypes = {"markdown", "text"}}),
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.formatting.lua_format,
-        null_ls.builtins.formatting.isort, null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.flake8
-    }
-    null_ls.config({sources = sources})
+  local null_ls = require "null-ls"
+  local sources = {
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.diagnostics.write_good.with { filetypes = { "markdown", "text" } },
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.flake8,
+  }
+  null_ls.config { sources = sources }
 
-    lsputils.setup_server("null-ls", CONFIG)
+  lsputils.setup_server("null-ls", CONFIG)
 end
 
 return M
