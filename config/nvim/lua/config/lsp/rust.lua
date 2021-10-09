7015ea68a4e4844e9cfc4a41aa2786a13cc47d43
alpha2phi <alpha2phi@gmail.com>
2021-10-09 15:16:50 +0800

    Fix code lens

--- config/nvim/lua/config/lsp/rust.lua
+++ config/nvim/lua/config/lsp/rust.lua
@@ -1,26 +1,32 @@
 local M = {}
 
-local lsputils = require("config.lsp.utils")
+local lsputils = require "config.lsp.utils"
 
 CONFIG = {}
 
 function M.setup()
-    require('rust-tools').setup({
-        server = {
-            capabilities = lsputils.get_capabilities(),
-            on_attach = lsputils.lsp_attach,
-            on_init = lsputils.lsp_init,
-            on_exit = lsputils.lsp_exit,
-            flags = {debounce_text_changes = 150}
-        }
-    })
+  require("rust-tools").setup {
+    tools = {
+      hover_actions = {
+        auto_focus = true,
+      },
+    },
+    server = {
+      capabilities = lsputils.get_capabilities(),
+      on_attach = lsputils.lsp_attach,
+      on_init = lsputils.lsp_init,
+      on_exit = lsputils.lsp_exit,
+      flags = { debounce_text_changes = 150 },
+    },
+  }
 
-    M.autocmds()
-    M.keymappings()
+  M.autocmds()
+  M.keymappings()
 end
 
 function M.autocmds()
-    vim.api.nvim_exec([[
+  vim.api.nvim_exec(
+    [[
 
       augroup RUST
         autocmd!
@@ -29,34 +35,36 @@ function M.autocmds()
         autocmd BufEnter *.rs lua require("config.lsp.rust").keymappings()
       augroup END
 
-    ]], false)
+    ]],
+    false
+  )
 end
 
 function M.keymappings()
-    local opts = {
-        mode = "n",
-        prefix = "<leader>",
-        buffer = nil,
-        silent = false,
-        noremap = true,
-        nowait = true
-    }
+  local opts = {
+    mode = "n",
+    prefix = "<leader>",
+    buffer = nil,
+    silent = false,
+    noremap = true,
+    nowait = true,
+  }
 
-    local wk = require("which-key")
-    local mappings = {
-        ["r"] = {
-            name = "Run",
-            r = {":update<CR>:RustRun<CR>", "Rust run"},
-            d = {":update<CR>:RustDebuggables<CR>", "Rust debuggables"},
-            w = {
-                ":update<CR>:sp term://cargo watch -s 'clear && cargo run -q'<CR>",
-                "Cargo watch"
-            },
-            c = {":update<CR>:Cargo run<CR>", "Cargo run"},
-            l = {":RustRunnables<CR>", "Rust runnables"}
-        }
-    }
-    wk.register(mappings, opts)
+  local wk = require "which-key"
+  local mappings = {
+    ["r"] = {
+      name = "Run",
+      r = { ":update<CR>:RustRun<CR>", "Rust run" },
+      d = { ":update<CR>:RustDebuggables<CR>", "Rust debuggables" },
+      w = {
+        ":update<CR>:sp term://cargo watch -s 'clear && cargo run -q'<CR>",
+        "Cargo watch",
+      },
+      c = { ":update<CR>:Cargo run<CR>", "Cargo run" },
+      l = { ":RustRunnables<CR>", "Rust runnables" },
+    },
+  }
+  wk.register(mappings, opts)
 end
 
 return M
