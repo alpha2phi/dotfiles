20fd97a0764f4df690c99890d4ce4d8fbe79e867
alpha2phi <alpha2phi@gmail.com>
2021-12-29 16:45:32 +0800

    Update defaults

--- config/nvim/after/plugin/defaults.lua
+++ config/nvim/after/plugin/defaults.lua
@@ -43,19 +43,19 @@ function M.setup()
   wo.cursorline = true
   opt.pumblend = 17
 
-  opt.formatoptions:append "cqnj"
-  opt.formatoptions:remove "ator2"
+  -- opt.formatoptions:append "cqnj"
+  -- opt.formatoptions:remove "ator2"
 
-  -- opt.formatoptions = opt.formatoptions
-  --   - "a" -- Auto formatting is BAD.
-  --   - "t" -- Don't auto format my code. I got linters for that.
-  --   + "c" -- In general, I like it when comments respect textwidth
-  --   + "q" -- Allow formatting comments w/ gq
-  --   - "o" -- O and o, don't continue comments
-  --   - "r" -- Don't insert comment after <Enter>
-  --   + "n" -- Indent past the formatlistpat, not underneath it.
-  --   + "j" -- Auto-remove comments if possible.
-  --   - "2" -- I'm not in gradeschool anymore
+  opt.formatoptions = opt.formatoptions
+    - "a" -- Auto formatting is BAD.
+    - "t" -- Don't auto format my code. I got linters for that.
+    + "c" -- In general, I like it when comments respect textwidth
+    + "q" -- Allow formatting comments w/ gq
+    - "o" -- O and o, don't continue comments
+    - "r" -- Don't insert comment after <Enter>
+    + "n" -- Indent past the formatlistpat, not underneath it.
+    + "j" -- Auto-remove comments if possible.
+    - "2" -- I'm not in gradeschool anymore
 
   -- g.virtualedit = "all"
   g.python3_host_prog = "~/miniconda3/bin/python3"
