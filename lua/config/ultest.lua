737176f9092c570db6b37c4d5219664aa0f98c73
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-01-16 21:42:56 -0400

    fix(test): syncing ts patterns with js (tpl); ultest;

--- lua/config/ultest.lua
+++ lua/config/ultest.lua
@@ -1,2 +1,3 @@
 vim.cmd('let test#javascript#jest#options = "--color=always"')
 vim.cmd('let test#javascript#reactscripts#options = "--watchAll=false"')
+vim.cmd("let g:test#typescript#patterns = g:test#javascript#patterns")
