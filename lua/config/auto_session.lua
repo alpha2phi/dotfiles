bd103730803364a30eee9bff139e281d092d93e4
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-01-16 21:34:00 -0400

    fix(layout): auto-session to not create empty windows because of trouble

--- lua/config/auto_session.lua
+++ lua/config/auto_session.lua
@@ -10,10 +10,10 @@ function config.setup()
 		auto_save_enabled = true,
 		auto_restore_enabled = true,
 		auto_session_suppress_dirs = false,
-		pre_save_cmds = { "TroubleToggle" },
-		post_save_cmds = { "TroubleToggle" },
-		-- pre_restore_cmds = { "MinimapClose" },
-		-- post_restore_cmds = { "Minimap" },
+		pre_save_cmds = { "TroubleClose" },
+		post_save_cmds = { "Trouble" },
+		pre_restore_cmds = { "TroubleClose" },
+		post_restore_cmds = { "Trouble" },
 	}
 
 	require("auto-session").setup(opts)
