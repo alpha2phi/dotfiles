418bd5ae4a512a4fec391ba4e1105078c91359dd
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-05-31 13:13:00 -0400

    deprecation cfg updates

--- lua/config/diffview.lua
+++ lua/config/diffview.lua
@@ -15,9 +15,11 @@ function config.setup()
 			fold_open = "",
 		},
 		file_panel = {
-			position = "left", -- One of 'left', 'right', 'top', 'bottom'
-			width = 35, -- Only applies when position is 'left' or 'right'
-			height = 10, -- Only applies when position is 'top' or 'bottom'
+			win_config = {
+				position = "left", -- One of 'left', 'right', 'top', 'bottom'
+				width = 35, -- Only applies when position is 'left' or 'right'
+				height = 10, -- Only applies when position is 'top' or 'bottom'
+			},
 			listing_style = "tree", -- One of 'list' or 'tree'
 			tree_options = { -- Only applies when listing_style is 'tree'
 				flatten_dirs = true,
@@ -25,9 +27,11 @@ function config.setup()
 			},
 		},
 		file_history_panel = {
-			position = "bottom",
-			width = 35,
-			height = 16,
+			win_config = {
+				position = "bottom",
+				width = 35,
+				height = 21,
+			},
 			log_options = {
 				max_count = 256, -- Limit the number of commits
 				follow = false, -- Follow renames (only for single file)
