d2f2c873074b5ffa1c8d5d6e38cd17eaa5740d5d
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-08-17 12:17:35 -0400

    chore(color): more color updates

--- lua/utils/color.lua
+++ lua/utils/color.lua
@@ -11,14 +11,23 @@ local M = {
   hsluv = lush.hsluv,
 }
 
-function M.vim.background_blend(color, strength)
-  local bg_color = vim.api.nvim_get_hl_by_name("Normal", true).background or "3213215"
+function M.vim.background_blend(color, strength, hl)
+  Bg_color = nil
+  status, hl_cfg = pcall(function()
+    return vim.api.nvim_get_hl_by_name(hl, true)
+  end)
+  if hl and status then
+    Bg_color = hl_cfg.background or vim.api.nvim_get_hl_by_name("Normal", true).background
+  else
+    Bg_color = vim.api.nvim_get_hl_by_name("Normal", true).background
+  end
   local blend_color = lush.hsl(color)
+  local base_color = Bg_color and ("#" .. string.format("%06x", Bg_color)) or my_colors[vim.opt.background:get()]
 
-  return lush.hsl("#" .. string.format("%06x", bg_color)).mix(blend_color, strength).hex
+  return lush.hsl(base_color).mix(blend_color, strength).hex
 end
 
-function M.vim.highlight_blend_bg(hl_name, strength, color, full)
+function M.vim.highlight_blend_bg(hl_name, strength, color)
   local old_hl = nil
   local fetch_old_hl = function()
     if color == nil then
@@ -28,8 +37,8 @@ function M.vim.highlight_blend_bg(hl_name, strength, color, full)
   end
 
   if pcall(fetch_old_hl) or color then
-    local hl_guibg = M.vim.background_blend(color or ("#" .. string.format("%06x", old_hl)), strength)
-    vim.api.nvim_set_hl(0, hl_name, { background = hl_guibg })
+    local hl_guibg = M.vim.background_blend(color or ("#" .. string.format("%06x", old_hl)), strength, "MyNormal")
+    vim.api.nvim_set_hl(0, hl_name, { background = hl_guibg, nocombine = false })
   end
 end
 
