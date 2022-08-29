e2d3f99bbfbd8fa2d9ded4ab379cc8a21d4ec6e1
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-08-28 22:43:30 -0400

    feat(convenience): zen mode setup tweaks

--- lua/config/zen.lua
+++ lua/config/zen.lua
@@ -1,25 +1,25 @@
-local config = {}
+local M = {}
 
-function config.setup()
+function M.setup()
   require("zen-mode").setup({
     window = {
-      backdrop = 0.97, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+      backdrop = 0.9, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
       -- height and width can be:
       -- * an absolute number of cells when > 1
       -- * a percentage of the width / height of the editor when <= 1
       -- * a function that returns the width or the height
       width = 150, -- width of the Zen window
-      height = 1, -- height of the Zen window
+      height = vim.api.nvim_win_get_height(0) - 2,
       -- by default, no options are changed for the Zen window
       -- uncomment any of the options below, or add other vim.wo options you want to apply
       options = {
-        signcolumn = "yes", -- disable signcolumn
-        -- number = false, -- disable number column
-        -- relativenumber = false, -- disable relative numbers
-        -- cursorline = false, -- disable cursorline
-        -- cursorcolumn = false, -- disable cursor column
-        -- foldcolumn = "0", -- disable fold column
-        -- list = false, -- disable whitespace characters
+        signcolumn = "auto:3", -- disable signcolumn
+        number = true, -- disable number column
+        relativenumber = true, -- disable relative numbers
+        cursorline = true, -- disable cursorline
+        cursorcolumn = true, -- disable cursor column
+        foldcolumn = "auto:3", -- disable fold column
+        list = true, -- disable whitespace characters
       },
     },
     plugins = {
@@ -27,28 +27,33 @@ function config.setup()
       -- comment the lines to not apply the options
       options = {
         enabled = true,
-        -- ruler = false, -- disables the ruler text in the cmd line area
-        showcmd = true, -- disables the command in the last line of the screen
+        ruler = false, -- disables the ruler text in the cmd line area
+        showcmd = false, -- disables the command in the last line of the screen
       },
       twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
-      gitsigns = { enabled = true }, -- disables git signs
+      gitsigns = { enabled = false }, -- disables git signs
       tmux = { enabled = false }, -- disables the tmux statusline
       -- this will change the font size on kitty when in zen mode
       -- to make this work, you need to set the following kitty options:
       -- - allow_remote_control socket-only
       -- - listen_on unix:/tmp/kitty
-      -- kitty = {
-      -- 	enabled = false,
-      -- 	font = "+4", -- font size increment
-      -- },
+      kitty = {
+        enabled = true,
+        font = "+4", -- font size increment
+      },
     },
     -- callback where you can add custom code when the Zen window opens
     on_open = function(win)
-      vim.api.nvim_win_set_config(win, { zindex = 51 })
+      vim.cmd("set showtabline=0")
+      vim.cmd("e")
+      vim.cmd("set cmdheight=0")
+      -- vim.api.nvim_win_set_config(win, { relative = 'win', win = win, bufpos = { -1, 0 }, row = -1 })
     end,
     -- callback where you can add custom code when the Zen window closes
-    -- on_close = function() end,
+    on_close = function()
+      vim.cmd("set showtabline=1")
+    end,
   })
 end
 
-return config
+return M
