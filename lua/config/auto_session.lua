19887f08a057ef0e3d16d81ad41ef5258c940841
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-08-28 22:41:14 -0400

    chore(autosession): tweak

--- lua/config/auto_session.lua
+++ lua/config/auto_session.lua
@@ -4,7 +4,7 @@ local M = {}
 function M.setup()
   local opts = {
     log_level = "error",
-    auto_session_enable_last_session = false,
+    auto_session_enable_last_session = true,
     auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
     auto_session_enabled = true,
     auto_save_enabled = true,
@@ -17,9 +17,7 @@ function M.setup()
     post_restore_cmds = {},
   }
 
-  vim.defer_fn(function()
-    require("auto-session").setup(opts)
-  end, 1000)
+  require("auto-session").setup(opts)
 end
 
 return M
